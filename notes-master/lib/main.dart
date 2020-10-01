55345e28bd8af6a1578ea64ea126ce8050799651
JoshuaVela <62628649+JoshuaVela@users.noreply.github.com>
2020-09-30 23:03:18 -0500

    nota
    tyghj

---
+++ notes-master/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'new_note.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyHomePage(title: 'Notas'),
+        '/newnote': (context) => new_note_Screen(),
+      },
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
